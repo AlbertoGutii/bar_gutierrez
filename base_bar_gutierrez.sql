4a09c75df9cdd4edd76da641779ab6b254cb23d7
Alberto <agutierreza31@educantabria.es>
2024-03-18 13:54:38 +0100

    Creacion base de datos

---
+++ base_bar_gutierrez.sql
@@ -0,0 +1,113 @@
+-- Creamos la base de datos
+CREATE DATABASE IF NOT EXISTS bar_gutierrez;
+USE bar_gutierrez;
+
+-- Creamos las tablas
+
+-- TABLA TIPOS_PRODUCTO
+CREATE TABLE IF NOT EXISTS tipos_producto (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    categoria CHAR(20) NOT NULL
+) ENGINE = INNODB;
+
+-- TABLA TIPOS_VINO
+CREATE TABLE IF NOT EXISTS tipos_vino (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    categoria CHAR(20) NOT NULL
+) ENGINE = INNODB;
+
+-- TABLA MESAS
+CREATE TABLE IF NOT EXISTS mesas (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre CHAR(20) NOT NULL,
+    estado BINARY NOT NULL,
+    capacidad INT NOT NULL
+) ENGINE = INNODB;
+
+-- TABLA PRODUCTOS
+CREATE TABLE IF NOT EXISTS productos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre CHAR(30) NOT NULL,
+    descripcion CHAR(50) NOT NULL,
+    foto CHAR(80) NOT NULL,
+    precio INT NOT NULL
+) ENGINE = INNODB;
+
+-- TABLA VINOS
+CREATE TABLE IF NOT EXISTS vinos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre CHAR(30) NOT NULL,
+    descripcion CHAR(50) NOT NULL,
+    foto CHAR(80) NOT NULL,
+    precio INT NOT NULL
+) ENGINE = INNODB;
+
+-- TABLA CLIENTES
+CREATE TABLE IF NOT EXISTS clientes (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ADMIN BINARY(1) NOT NULL,
+    nombre CHAR(30) NOT NULL,
+    apellidos CHAR(40) NOT NULL,
+    telefono CHAR(9) NOT NULL,
+    email CHAR(50) NOT NULL UNIQUE,
+    contraseña CHAR(255) NOT NULL
+) ENGINE = INNODB;
+
+-- TABLA PEDIDOS
+CREATE TABLE IF NOT EXISTS pedidos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_cliente INT NOT NULL,
+    hora_recogida DATETIME NOT NULL,
+    observaciones CHAR(50) NULL,
+    precio INT NOT NULL,
+    entregado BINARY(1) NOT NULL,
+    FOREIGN KEY (fk_cliente) REFERENCES clientes (id)
+    ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+-- TABLA CATEGORIAS_PRODUCTOS
+CREATE TABLE IF NOT EXISTS categorias_productos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_productos INT NOT NULL,
+    fk_categoria INT NOT NULL,
+    FOREIGN KEY (fk_productos) REFERENCES productos (id)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (fk_categoria) REFERENCES tipos_producto (id)
+    ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+-- TABLA CATEGORIAS_VINOS
+CREATE TABLE IF NOT EXISTS categorias_vinos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_vinos INT NOT NULL,
+    fk_categoria INT NOT NULL,
+    FOREIGN KEY (fk_vinos) REFERENCES vinos (id)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (fk_categoria) REFERENCES tipos_vino (id)
+    ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+-- TABLA DETALLE_PEDIDO
+CREATE TABLE IF NOT EXISTS detalle_pedido (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_pedido INT NOT NULL,
+    fk_producto INT NOT NULL,
+    observaciones CHAR(50) NULL,
+    FOREIGN KEY (fk_pedido) REFERENCES pedidos (id)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (fk_producto) REFERENCES productos (id)
+    ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+-- TABLA RESERVAS
+CREATE TABLE IF NOT EXISTS reservas (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fk_cliente INT NOT NULL,
+    fk_mesa INT NOT NULL,
+    num_personas INT NOT NULL,
+    fecha DATETIME NOT NULL,
+    FOREIGN KEY (fk_cliente) REFERENCES clientes (id)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (fk_mesa) REFERENCES mesas (id)
+    ON UPDATE CASCADE
+) ENGINE = INNODB;
---
+++ inserciones_basicas.sql
@@ -0,0 +1,14 @@
+-- TABLA TIPOS_PRODUCTOS
+INSERT INTO tipos_productos (categoria) VALUES ('Entrantes calientes');
+INSERT INTO tipos_productos (categoria) VALUES ('Entrantes frios');
+INSERT INTO tipos_productos (categoria) VALUES ('Carnes');
+INSERT INTO tipos_productos (categoria) VALUES ('Guisos caseros');
+INSERT INTO tipos_productos (categoria) VALUES ('Pescados');
+
+-- TABLA TIPOS_VINOS
+INSERT INTO tipos_vinos (categoria) VALUES ('Claro Ribera del Duero');
+INSERT INTO tipos_vinos (categoria) VALUES ('Blanco Rueda y Verdejo');
+INSERT INTO tipos_vinos (categoria) VALUES ('Tinto Ribera del Duero');
+INSERT INTO tipos_vinos (categoria) VALUES ('Crianza Ribera del Duero');
+INSERT INTO tipos_vinos (categoria) VALUES ('Otras opciones');
+
