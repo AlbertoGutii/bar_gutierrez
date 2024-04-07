8b0e7d16b9bbb677a8f60f9670dbf9a3a7c93039
Alberto <a.g.arauzo13@gmail.com>
2024-04-07 13:58:24 +0200

    carrusell casi terminado, footer añadido, enlaces legales tambien, falta terminar estilo footer y poner mas colores en la pagina

---
+++ JS/gestion_vinos.js
@@ -0,0 +1,26 @@
+
+// CAMBIAR RUTA DE LOS BOTONES
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("btnInicio").onclick = function() {
+        window.location.href = "../bar_gutierrez/pagina_principal.html";
+    };
+    
+    document.getElementById("btnCarta").onclick = function() {
+        window.location.href = "../bar_gutierrez/paginas_carta/carta.html";
+    };
+    document.getElementById("btnMenu").onclick = function() {
+        window.location.href = "../bar_gutierrez/paginas_carta/menu.html";
+    };
+    
+    document.getElementById("btnVinos").onclick = function() {
+        window.location.href = "../bar_gutierrez/paginas_carta/vinos.html";
+    };
+
+    document.getElementById("btnContacto").onclick = function() {
+        window.location.href = "../bar_gutierrez/contacto.html";
+    };
+
+    document.getElementById("btnReserva").onclick = function() {
+        window.location.href = "../admin/admin_gestionar_productos.html";
+    };
+});
