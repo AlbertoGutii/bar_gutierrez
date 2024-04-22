02c11ce6140cd5a100dca131b975dc5b0f2ec5ed
AlbertoGutii <159535824+AlbertoGutii@users.noreply.github.com>
2024-04-22 19:34:42 +0200

    cambios en rutas

--- JS/gestion_vinos.js
+++ JS/gestion_vinos.js
@@ -2,26 +2,26 @@
 // CAMBIAR RUTA DE LOS BOTONES
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btnLogo").onclick = function() {
-        window.location.href = "../bar_gutierrez/index.html";
+        window.location.href = "../index.html";
     };
 
     document.getElementById("btnInicio").onclick = function() {
-        window.location.href = "../bar_gutierrez/index.html";
+        window.location.href = "../index.html";
     };
     
     document.getElementById("btnCarta").onclick = function() {
-        window.location.href = "../bar_gutierrez/paginas_carta/carta.html";
+        window.location.href = "../paginas_carta/carta.html";
     };
     document.getElementById("btnMenu").onclick = function() {
-        window.location.href = "../bar_gutierrez/paginas_carta/menu.html";
+        window.location.href = "../paginas_carta/menu.html";
     };
     
     document.getElementById("btnVinos").onclick = function() {
-        window.location.href = "../bar_gutierrez/paginas_carta/vinos.html";
+        window.location.href = "../paginas_carta/vinos.html";
     };
 
     document.getElementById("btnContacto").onclick = function() {
-        window.location.href = "../bar_gutierrez/contacto.html";
+        window.location.href = "../contacto.html";
     };
 
     document.getElementById("btnReserva").onclick = function() {
