e8bc5ee7d99e9cb1d6d96b87efec5972d7342851
Alberto <a.g.arauzo13@gmail.com>
2024-05-20 14:08:17 +0200

    Añadimos algunas excepciones para el inicio de sesion en la pagina de inicio, y ocultamos algunos botones dependiendo de si el usuario ha iniciado sesion o no y dejamos otros como la cesta pero al pulsarlo si no has iniciado sesion te lleva al login

--- JS/usuario_inicio.js
+++ JS/usuario_inicio.js
@@ -1,4 +1,7 @@
-window.onload = principal
+window.onload = function() {
+    principal()
+    comprobarExisteEmail()
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btnLogo").onclick = function() {
@@ -8,15 +11,15 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btnInicio").onclick = function() {
         window.location.href = "./index.html"
     }
-    
+
     document.getElementById("btnCarta").onclick = function() {
         window.location.href = "./paginas_usuarios/usuario/carta.html"
     }
-    
+
     document.getElementById("btnMenu").onclick = function() {
         window.location.href = "./paginas_usuarios/usuario/menu.html"
     }
-    
+
     document.getElementById("btnVinos").onclick = function() {
         window.location.href = "./paginas_usuarios/usuario/vinos.html"
     }
@@ -33,6 +36,10 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "./paginas_usuarios/login.html"
     }
 
+    document.getElementById("btnCesta").onclick = function() {
+        window.location.href = "./paginas_usuarios/cesta.html"
+    }
+
     document.getElementById("btnHistorialPedidos").onclick = function() {
         window.location.href = "./paginas_usuarios/historial_pedidos.html"
     }
@@ -40,10 +47,14 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btnAdministrador").onclick = function() {
         window.location.href = "./paginas_usuarios/administrador/inicio_administrador.html"
     }
+
+    document.getElementById("btnCerrarSesion").onclick = function() {
+        localStorage.clear()
+        window.location.href = "./index.html"
+    }
 })
 
-function principal()
-{
+function principal() {
     iniciarCarrusel()
 }
 
@@ -51,18 +62,86 @@ function iniciarCarrusel() {
     let slideIndex = 0
     let slides = document.getElementsByClassName('slide')
 
-    // Ocultar todos los slides excepto el primero
     for (let i = 1; i < slides.length; i++) {
         slides[i].style.display = "none"
     }
 
-    // Función para mostrar el próximo slide
     function mostrarSiguienteSlide() {
         slides[slideIndex].style.display = "none"
         slideIndex = (slideIndex + 1) % slides.length
         slides[slideIndex].style.display = "flex"
     }
 
-    // Iniciar el carrusel automáticamente
     setInterval(mostrarSiguienteSlide, 2500)
+}
+
+function comprobarExisteEmail() {
+    let miEmail = localStorage.getItem("email")
+    if (!miEmail) {
+        redirigirLogin()
+        return
+    }
+
+    let miPeticion = new XMLHttpRequest()
+
+    miPeticion.open("POST", "./PHP/redireccion.php", true)
+
+    miPeticion.onreadystatechange = function() {
+        if (miPeticion.readyState == 4 && miPeticion.status == 200) {
+            console.log("existe", miPeticion.responseText)
+            if(miPeticion.responseText === "0") {
+                redirigirLogin()
+            } else {
+                mostrarBotonesSesion(true)
+                comprobarEsAdmin()
+            }
+        }
+    }
+
+    miPeticion.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    let datos = "comprobarExisteEmail=" + miEmail
+    miPeticion.send(datos)
+}
+
+function comprobarEsAdmin() {
+    let miEmail = localStorage.getItem("email")
+    let miPeticion = new XMLHttpRequest()
+
+    miPeticion.open("POST", "./PHP/redireccion.php", true)
+
+    miPeticion.onreadystatechange = function() {
+        if (miPeticion.readyState == 4 && miPeticion.status == 200) {
+            console.log("es admin: ", miPeticion.responseText)
+            if (miPeticion.responseText === "1") {
+                document.getElementById("btnAdministrador").style.display = "block"
+            } else {
+                document.getElementById("btnAdministrador").style.display = "none"
+            }
+        }
+    }
+
+    miPeticion.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    let datos = "comprobarEsAdmin=" + miEmail
+    console.log(datos)
+    miPeticion.send(datos)
+}
+
+function redirigirLogin() {
+    document.getElementById("btnCesta").onclick = function() {
+        window.location.href = "./paginas_usuarios/login.html"
+    }
+    document.getElementById("btnAdministrador").style.display = "none"
+    mostrarBotonesSesion(false)
+}
+
+function mostrarBotonesSesion(haIniciadoSesion) {
+    if (haIniciadoSesion) {
+        document.getElementById("btnIniciarSesion").style.display = "none"
+        document.getElementById("btnCerrarSesion").style.display = "block"
+        document.getElementById("btnHistorialPedidos").style.display = "block"
+    } else {
+        document.getElementById("btnIniciarSesion").style.display = "block"
+        document.getElementById("btnCerrarSesion").style.display = "none"
+        document.getElementById("btnHistorialPedidos").style.display = "none"
+    }
 }
